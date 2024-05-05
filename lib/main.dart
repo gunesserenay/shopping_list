724f831c12e850324b8ff7fba34fadfa1c6bfd2e
Serenay <serenayygunes@gmail.com>
2024-05-05 19:01:05 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+final theme = ThemeData(
+    useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+     seedColor: const Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 42, 51, 59),
+  ),
+ // textTheme: GoogleFonts.latoTextTheme(),
+);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Groceries',
+      theme: theme,
+      home: ...,
+    );
+  }
+}
