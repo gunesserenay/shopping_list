ea36fa15352d17c93cb77e598a569756a4a3b70e
Serenay <serenayygunes@gmail.com>
2024-05-05 20:48:51 +0300

    New item screen added

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (ctx) => const NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
       body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          ListTile(
-            iconColor: Colors.yellow,
-            title: Text(groceryItems[index].name.toString()),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: groceryItems[index].category.color,
-            ),
-            trailing: Text(groceryItems[index].quantity.toString()),
-          );
-        },
-      ),
+          itemCount: groceryItems.length,
+          itemBuilder: (ctx, index) => ListTile(
+                iconColor: Colors.yellow,
+                title: Text(groceryItems[index].name.toString()),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: groceryItems[index].category.color,
+                ),
+                trailing: Text(groceryItems[index].quantity.toString()),
+              )),
     );
   }
 }
